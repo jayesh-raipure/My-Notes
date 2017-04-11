1d4378917f3eecfe73c32a0a47805716b9be2191
jayesh-raipure <jayesh.raipure@synerzip.com>
2017-04-11 12:52:42 +0530

    Update roman_to_int_conversion.rb

--- roman_to_int_conversion.rb
+++ roman_to_int_conversion.rb
@@ -2,16 +2,15 @@
 def roman_to_int(roman_num)
   i, j, result = 0, 0, 0
   roman_to_int_map =  {"I"=>1, "V"=>5, "X"=>10, "L"=>50, "C"=>100, "D"=>500, "M"=>1000}
-  num_length = roman_num.length-1;
-  roman_num.chars.each do
-    i = roman_to_int_map[roman_num[num_length]]
+  
+  roman_num.reverse.each_char do |num|
+    i = roman_to_int_map[num]
     if(i<j) then
       result -= i
     else
       result += i
     end 
     j = i
-    num_length -=1
   end 
   return result
 end
