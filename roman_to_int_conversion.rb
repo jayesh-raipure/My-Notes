6b11cb4785252f8ada6d3d2be792ee15c012d2e4
jayesh-raipure <jayesh.raipure@synerzip.com>
2017-04-24 15:44:26 +0530

    Create roman_to_int_conversion.rb

--- roman_to_int_conversion.rb
+++ roman_to_int_conversion.rb
@@ -1,20 +1,21 @@
 # Write a program to convert romal numeral to integer.
 def roman_to_int(roman_num)
-  i, j, result = 0, 0, 0
+  least_num, most_num, result = 0, 0, 0
   roman_to_int_map =  {"I"=>1, "V"=>5, "X"=>10, "L"=>50, "C"=>100, "D"=>500, "M"=>1000}
   
   roman_num.reverse.each_char do |num|
-    i = roman_to_int_map[num]
-    if(i<j) then
-      result -= i
-    else
-      result += i
-    end 
-    j = i
+    least_num = roman_to_int_map[num]
+    (least_num<most_num) ? result -= least_num : result += least_num
+    most_num = least_num
   end 
-  return result
+  "Interger Value of given Roman Number :#{result}"
 end
 
 print("Enter any Roman Number :")
-
-puts("Interger Value of given Roman Number : #{roman_to_int(gets.chomp.upcase)}")
+input_roman_num = gets.chomp.upcase
+if(/^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/ =~ input_roman_num) 
+  
+  puts("#{roman_to_int(input_roman_num)}")
+else
+  puts("Invalid Number Enterd.")
+end
